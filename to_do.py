03e535902a07405636907a225741e3aaf39d9189
JohnnieDiniz <diniz.vitor.alves@gmail.com>
2026-02-11 03:16:28 -0300

    Update to_do.py

--- to_do.py
+++ to_do.py
@@ -1,17 +1,1 @@
-# ideia principal é criar um gerenciador de tarefas de prioridade, com base o kanban
-
-def to_do():
-    alta = []
-    médio = []
-    baixo = []
-
-    proximo_id = 1
-    proximo_id += 1
-
-    print('Sistema de Tarefas')
-    print('Cadastro de tarefas')
-
-    prioridade = input('Nível de prioridade (alta/médio/baixo): ').strip().lower()
-
-    tarefa = input('Qual tarefa deseja cadastrar? ')
 
